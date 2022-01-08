62d2a5331f4918846f4cb2bd3b834725b8587b33
Daniel Pelagio Puch <19470231@campeche.tecnm.mx>
2022-01-07 20:22:01 -0600

    Agrega el manejo de eventos que da comportamiento a la interfaz de usuario de la aplicación

--- ImcApp/ImcApp/MainPage.xaml.cs
+++ ImcApp/ImcApp/MainPage.xaml.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ImcApp.Models;
+using System;
 using Xamarin.Forms;
+
+
 
 namespace ImcApp
 {
@@ -13,8 +11,74 @@ namespace ImcApp
         public MainPage()
         {
             InitializeComponent();
+            LimpiarIU();
         }
 
-        
+        private void LimpiarIU()
+        {
+            PesoEntry.Text = string.Empty;
+            EstaturaEntry.Text = string.Empty;
+            ImcLabel.Text = string.Empty;
+            SituacionNutricionalLabel.Text = string.Empty;
+            
+        }
+
+        private string GetEstadoNutricional(CalculadoraImc.EstadoNutricional estado)
+        {
+            string situacion;
+            switch(estado)
+            {
+                case CalculadoraImc.EstadoNutricional.PesoBajo:
+                    situacion = "Peso bajo";
+                    break;
+                case CalculadoraImc.EstadoNutricional.PesoNormal:
+                    situacion = "Peso normal";
+                    break;
+                case CalculadoraImc.EstadoNutricional.SobrePeso:
+                    situacion = "Sobrepeso";
+                    break;
+                case CalculadoraImc.EstadoNutricional.Obesidad:
+                    situacion = "Obesidad";
+                    break;
+                case CalculadoraImc.EstadoNutricional.ObesidadExtrema:
+                    situacion = "Obesidad extrema";
+                    break;
+                default: 
+                    situacion = string.Empty;
+                    break;
+            }
+            return situacion;
+        }
+
+
+        private void CalcularButton_Clicked(object sender, EventArgs e)
+        {
+            decimal peso; 
+            decimal estatura;
+            decimal imc;
+            CalculadoraImc.EstadoNutricional estadoNutricional;
+            bool pesoEsConvertible = decimal.TryParse(PesoEntry.Text, out peso);
+            bool estaturaEsConvertible = decimal.TryParse(EstaturaEntry.Text, out estatura);
+            if (pesoEsConvertible && estaturaEsConvertible)
+            {
+                imc = CalculadoraImc.IndiceDeMasaCorporal(peso, estatura);
+                estadoNutricional = CalculadoraImc.SituacionNutricional(imc);
+                //ImcLabel.Text = imc.ToString();
+                ImcLabel.Text = string.Format("{0}", imc);
+                SituacionNutricionalLabel.Text = GetEstadoNutricional(estadoNutricional);
+            }
+            else
+            {
+                DisplayAlert(
+                    "Alerta", 
+                    "El peso y la estatura deben ser valores numéricos.",
+                    "Aceptar");
+            }
+        }
+
+        private void LimpiarButton_Clicked(object sender, EventArgs e)
+        {
+            LimpiarIU();
+        }
     }
 }
